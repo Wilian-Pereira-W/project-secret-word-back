1c384f06326f99655097e836d222d354bee4f8f4
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-21 17:00:41 -0300

    feat: implementado score service

---
+++ src/score/score.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateScoreDto } from './dto/create-score.dto';
+import { UpdateScoreDto } from './dto/update-score.dto';
+
+@Injectable()
+export class ScoreService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createScoreDto: CreateScoreDto) {
+    return 'This action adds a new score';
+  }
+
+  findAll() {
+    return `This action returns all score`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} score`;
+  }
+
+  update(id: number, updateScoreDto: UpdateScoreDto) {
+    return `This action updates a #${id} score`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} score`;
+  }
+}
