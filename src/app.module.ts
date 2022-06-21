64b813b0969385140a11340c0c02468856f4643c
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-21 16:57:23 -0300

    feat: import do UserModule, ScoreModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ScoreModule } from './score/score.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ScoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
