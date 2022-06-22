d7cebe4c46ba80abd5e36bfcf4e5adfffcd6dac7
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-22 20:57:40 -0300

    fix(auth): rota login publico

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, HttpCode, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { IsPublic } from './decorators/is-public.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/AuthRequest';
 
@@ -7,11 +8,11 @@ import { AuthRequest } from './models/AuthRequest';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @IsPublic()
   @Post('login')
   @UseGuards(LocalAuthGuard)
   @HttpCode(200)
   login(@Request() req: AuthRequest) {
-    console.log(req.user);
     return this.authService.login(req.user);
   }
 }
