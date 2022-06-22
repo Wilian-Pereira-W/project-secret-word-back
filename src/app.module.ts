b10949721e11150e3af08e4cf59ca1b38aa3918d
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-22 11:52:55 -0300

    feat: importando AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ScoreModule } from './score/score.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, ScoreModule, PrismaModule],
+  imports: [UserModule, ScoreModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
