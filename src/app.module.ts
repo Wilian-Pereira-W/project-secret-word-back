24bdf525d40e957acc6c9c7f412563ca6389badb
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-22 11:06:21 -0300

    feat: importando PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ScoreModule } from './score/score.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, ScoreModule],
+  imports: [UserModule, ScoreModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
