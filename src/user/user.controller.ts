df61620dd9b4ccd086f5b1a856b6692591caead0
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-21 22:08:40 -0300

    feat: implementado o create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,23 +6,47 @@ import {
   Patch,
   Param,
   Delete,
+  Res,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Response } from 'express';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  async create(@Res() res: Response, @Body() data: CreateUserDto) {
+    try {
+      const user = await this.userService.create(data);
+      console.log(user);
+
+      if (user === 'Esse email já existe') {
+        return res.status(409).json({ message: 'Esse email já existe' });
+      }
+
+      if (user === 'Esse nick já existe') {
+        return res.status(409).json({ message: 'Esse nick já existe' });
+      }
+
+      if (user) {
+        return res.status(200).json(user);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   @Get()
   findAll() {
     return this.userService.findAll();
+  }
+
+  @Get()
+  findLogin(@Body() password: string, email: string) {
+    return this.userService.findLogin(password, email);
   }
 
   @Get(':id')
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,18 +2,56 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as md5 from 'md5';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
-  create(data: CreateUserDto) {
-    const user = this.prisma.user.create({ data });
-    return user;
+  async create(data: CreateUserDto) {
+    const { name, nick, password, email } = data;
+    const encryptedPass = md5(password);
+
+    const emailExists = await this.prisma.user.findMany({
+      where: {
+        email,
+      },
+    });
+    if (emailExists.length !== 0) return 'Esse email já existe';
+
+    const nickExists = await this.prisma.user.findMany({
+      where: {
+        nick,
+      },
+    });
+
+    if (nickExists.length !== 0) return 'Esse nick já existe';
+
+    if (emailExists.length === 0 && nickExists.length === 0) {
+      const user = this.prisma.user.create({
+        data: {
+          name,
+          email,
+          password: encryptedPass,
+          nick,
+        },
+      });
+      return user;
+    }
   }
 
   findAll() {
     const users = this.prisma.user.findMany();
     return users;
+  }
+
+  findLogin(password: string, email: string) {
+    const user = this.prisma.user.findMany({
+      where: {
+        email,
+      },
+    });
+    console.log(user);
+    return user;
   }
 
   findOne(id: number) {
