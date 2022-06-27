4fff13eae64dd75357b5a435d1d6dfd495825ed6
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-27 20:57:30 -0300

    fix: alterando relacionamento das tabelas

--- prisma/migrations/20220627221845_init/migration.sql
+++ prisma/migrations/20220627221845_init/migration.sql
@@ -14,7 +14,7 @@ CREATE TABLE `User` (
 -- CreateTable
 CREATE TABLE `Score` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `userId` INTEGER NOT NULL,
+    `authorId` INTEGER NOT NULL,
     `score` INTEGER NOT NULL,
     `difficulty` VARCHAR(191) NOT NULL,
 
@@ -22,4 +22,4 @@ CREATE TABLE `Score` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Score` ADD CONSTRAINT `Score_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Score` ADD CONSTRAINT `Score_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
